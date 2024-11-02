98baf4cfee264202ad74f179ca48c055884eaf52
Erian Flores <erian.flores@gmail.com>
2024-11-02 15:45:37 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import logo from "./../assets/logo-ironhack-blue.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-blue-600 text-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="flex justify-between h-20 items-center px-4">
         <div className="flex items-center space-x-2 w-1/4">
-
           {/* Home Button (Logo) */}
           <button className="flex items-center text-l py-1">
             <img src={logo} alt="Logo" className="h-8 w-auto" />
@@ -13,9 +13,7 @@ function Navbar() {
         </div>
 
         <div className="flex justify-center w-1/2">
-        <span className="text-xl">
-            Cohort Tools
-          </span>
+          <span className="text-xl">Cohort Tools</span>
         </div>
 
         <div className="w-1/4 flex justify-end mr-4">
@@ -26,8 +24,8 @@ function Navbar() {
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
+            <Link to="/profile">Profile</Link>
           </button>
-
         </div>
       </div>
     </nav>
